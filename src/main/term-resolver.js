6d8b2edc40e693d3d8e0729e746341aee40412d2
Arex-lbb <rex.1102@qq.com>
2026-07-11 15:11:42 +0800

    Add src/main/term-resolver.js

---
+++ src/main/term-resolver.js
@@ -0,0 +1,17 @@
+const { createClient } = require('./request-client.js');
+const { encryptQueryParams } = require('./sm2.js');
+
+async function resolveCurrentTermId(ytoken) {
+  const client = createClient(ytoken);
+  try {
+    const _j = encryptQueryParams({});
+    const resp = await client.get('/course-choose-overview/getParameters', { params: { _j } });
+    const data = resp.data?.data ?? resp.data ?? {};
+    const termId = String(data.termId ?? data.currentTermId ?? data.termCode ?? '');
+    if (!termId) throw new Error('未能解析当前学期 ID');
+    return { termId, label: data.termName ? String(data.termName) : undefined };
+  } catch {
+    return { termId: '124' };
+  }
+}
+module.exports = { resolveCurrentTermId };
